029f0d6d3afde333ce6373c3dad08247ea6019d2
rajsukanya <34818638+rajsukanya@users.noreply.github.com>
2020-05-13 01:28:14 -0400

    Update action.py

--- rasa/core/actions/action.py
+++ rasa/core/actions/action.py
@@ -558,8 +558,8 @@ class ActionSummarizeWithPassage(Action):
                             b[n] = 1
         if len(b) > 0:
             p = max(b, key=lambda x: x[1])
-            print('chosen sentiment: {}'.format(p[0]))
-            passage = self.choose_passage(p[0])
+            print('chosen sentiment: {}'.format(p))
+            passage = self.choose_passage(p)
             print('passage: -->{}<-->{}<-->{}<--'.format(passage[0], passage[1], passage[2])) 
             text = 'This conversation reminds of the passage {}. This was written by {}, in the work {}.'.format(passage[0], passage[1], passage[2])
         else:
